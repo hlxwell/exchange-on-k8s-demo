be6e1e26e4eb0bb4a27d473b1b61be86b71969b7
Michael He <hlxwell@gmail.com>
2019-05-28 09:28:38 +0800

    merge all seeds files

--- db-migrator/db/seeds.rb
+++ db-migrator/db/seeds.rb
@@ -1,12 +1,22 @@
-def load_seed(name)
-  require File.join(__dir__, "seeds", name + "_seeder")
-end
+# User ====================================
+User.register!("sample1@gmail.com", "fejkljr2ijfi")
+User.register!("sample2@gmail.com", "jd873kdufidj")
 
-load_seed "users"
-load_seed "deposites"
-load_seed "orders"
-load_seed "withdraws"
+# Deposite ===============================
+User.first.deposites.create!(currency: "btc", amount: 3)
+User.first.deposites.create!(currency: "jpy", amount: 15000)
+User.last.deposites.create!(currency: "btc", amount: 102)
+User.last.deposites.create!(currency: "jpy", amount: 3150)
 
+# Order ==================================
+User.first.buy_orders.create pair: "btcjpy", price: 120, volume: 110
+User.last.sell_orders.create! pair: "btcjpy", price: 115, volume: 100
+
+# Withdraw ==================================
+User.first.withdraws.create!(currency: "jpy", amount: 3000)
+User.last.withdraws.create!(currency: "btc", amount: 1.5)
+
+# Check balance
 puts "User 1 JPY: #{User.first.balance_in("jpy")}"
 puts "User 1 BTC: #{User.first.balance_in("btc")}"
 puts "User 2 JPY: #{User.last.balance_in("jpy")}"
--- db-migrator/db/seeds/withdraws_seeder.rb
+++
@@ -1,2 +0,0 @@
-User.first.withdraws.create!(currency: "jpy", amount: 3000)
-User.last.withdraws.create!(currency: "btc", amount: 1.5)
