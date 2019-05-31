abadf38715155df693ebb571adc26f02427cd8c0
Michael He <hlxwell@gmail.com>
2019-05-31 18:11:05 +0800

    add all auth service test

--- e2e-test/app.rb
+++ e2e-test/app.rb
@@ -4,6 +4,7 @@ require "active_record"
 require "json"
 require "yaml"
 
+# Load Data & Models
 config = YAML.load_file("database.yml")[ENV["RACK_ENV"]]
 ActiveRecord::Base.establish_connection(config)
 Dir["./models/*.rb"].each { |f| require f }
@@ -20,5 +21,30 @@ class APITest < Test::Unit::TestCase
     })
     @data = JSON.parse response.body
     assert_not_nil @data["id"]
+  end
+
+  def test_login
+    test_register_user
+    response = RestClient.post("http://localhost:9292/api/v1/sessions", {
+      "email" => "xxx@xxx.com",
+      "password" => "password",
+    })
+    @data = JSON.parse response.body
+    assert @data["token"].size >= 40
+  end
+
+  def test_token_verification
+    token = login_user
+    response = RestClient.get("http://localhost:9292/api/v1/sessions/#{token}/verify")
+    @data = JSON.parse response.body
+    assert_not_nil @data["email"]
+    assert_not_nil @data["id"]
+  end
+
+  protected
+
+  def login_user
+    User.register "xxx@xxx.com", "password"
+    User.login "xxx@xxx.com", "password"
   end
 end
