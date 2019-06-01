828457593590d826609cf9f1cb99ca56346523ff
Michael He <hlxwell@gmail.com>
2019-06-01 21:13:05 +0800

    fix seeds

--- db-migrator/db/seeds.rb
+++ db-migrator/db/seeds.rb
@@ -8,15 +8,23 @@ User.first.deposites.create!(currency: "jpy", amount: 15000)
 User.last.deposites.create!(currency: "btc", amount: 102)
 User.last.deposites.create!(currency: "jpy", amount: 3150)
 
-# Order ==================================
-User.first.buy_orders.create pair: "btcjpy", price: 120, volume: 110
-User.last.sell_orders.create! pair: "btcjpy", price: 115, volume: 100
+# Check balance
+puts "BEFORE ================"
+puts "User 1 JPY: #{User.first.balance_in("jpy")}"
+puts "User 1 BTC: #{User.first.balance_in("btc")}"
+puts "User 2 JPY: #{User.last.balance_in("jpy")}"
+puts "User 2 BTC: #{User.last.balance_in("btc")}"
+
+# # Order ==================================
+User.first.buy_orders.create pair: "btcjpy", price: 100, volume: 100
+User.last.sell_orders.create! pair: "btcjpy", price: 95, volume: 100
 
 # Withdraw ==================================
 User.first.withdraws.create!(currency: "jpy", amount: 3000)
 User.last.withdraws.create!(currency: "btc", amount: 1.5)
 
 # Check balance
+puts "AFTER ================"
 puts "User 1 JPY: #{User.first.balance_in("jpy")}"
 puts "User 1 BTC: #{User.first.balance_in("btc")}"
 puts "User 2 JPY: #{User.last.balance_in("jpy")}"
